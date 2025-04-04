80b0cb6647dc2e094fbf67b84c0cb1cabcec041d
Belar <belar@belardesign.com>
2025-04-04 08:36:48 +0000

    fix: app's background to show on macos

--- src/process/platform.js
+++ src/process/platform.js
@@ -6,9 +6,6 @@ export function applyDirectStyling() {
 	if (process.platform === "darwin") {
 		setTimeout(() => {
 			mainWindow.webContents.executeJavaScript(
-				`document.querySelector("body").style.backgroundColor = 'transparent'`,
-			);
-			mainWindow.webContents.executeJavaScript(
 				`document.querySelector("tab-group").shadowRoot.querySelector("nav").style.left = '80px'`,
 			);
 			mainWindow.webContents.executeJavaScript(
