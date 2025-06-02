8cca008a7cdf9212e1e13ec585197bd824331928
Belar <belar@belardesign.com>
2025-06-02 15:24:46 +0000

    feat: add download button

--- src/base/scripts/webviews/preload.mjs
+++ src/base/scripts/webviews/preload.mjs
@@ -3,6 +3,8 @@
 
 const { ipcRenderer } = require("electron");
 
+const BUTTON_DOWNLOAD_PROJECTS_ID = "download-projects";
+
 // Set the title of the tab name
 /// Instead of the tab name being "PROJECT_NAME - Penpot", this script will remove the " - Penpot" portion.
 function SetTitleToDash() {
@@ -84,10 +86,18 @@ window.onload = function () {
 	}
 };
 
-// Theme
-window.addEventListener("DOMContentLoaded", () =>
-	onClassChange(document.body, () => dispatchThemeUpdate()),
-);
+window.addEventListener("DOMContentLoaded", () => {
+	onClassChange(document.body, () => dispatchThemeUpdate());
+});
+
+navigation.addEventListener("navigate", (event) => {
+	const url = new URL(event.destination.url);
+	const isDashboard = url.hash.startsWith("#/dashboard");
+
+	if (isDashboard) {
+		prepareUI();
+	}
+});
 
 ipcRenderer.on("theme-request-update", () => dispatchThemeUpdate());
 
@@ -118,4 +128,67 @@ function onClassChange(node, callback) {
 function dispatchThemeUpdate() {
 	const isLightTheme = document.body.classList.contains("light");
 	ipcRenderer.sendToHost("theme-update", isLightTheme ? "light" : "dark");
+}
+
+async function prepareUI() {
+	const dashboardHeaderElement = await getElement(
+		"header.main_ui_dashboard_projects__dashboard-header",
+		{
+			maxRetries: 5,
+		},
+	);
+	const downloadButton = dashboardHeaderElement?.querySelector(
+		`#${BUTTON_DOWNLOAD_PROJECTS_ID}`,
+	);
+
+	if (dashboardHeaderElement && !downloadButton) {
+		const firstHeaderButton = dashboardHeaderElement.querySelector("button");
+
+		// Push header buttons to the right
+		dashboardHeaderElement.style.justifyContent = "flex-start";
+		dashboardHeaderElement.style.gap = "0.5rem";
+		firstHeaderButton.style.marginLeft = "auto";
+
+		const buttonElement = document.createElement("button");
+		buttonElement.id = BUTTON_DOWNLOAD_PROJECTS_ID;
+		buttonElement.innerText = "Download ALL projects";
+		buttonElement.classList.add(
+			"main_ui_dashboard_projects__btn-secondary",
+			"main_ui_dashboard_projects__btn-small",
+		);
+		dashboardHeaderElement.append(buttonElement);
+	}
+}
+
+/**
+ *
+ * @typedef {Object} Options
+ * @property {number} maxRetries
+ *
+ * @param {string} selector
+ * @param {Options} options
+ *
+ * @returns {HTMLElement | null}
+ */
+function getElement(selector, { maxRetries = 1 } = {}) {
+	let retriesCount = 0;
+
+	return new Promise((resolve) => {
+		const queryElement = () => {
+			const hasRetries = retriesCount <= maxRetries;
+
+			setTimeout(() => {
+				const element = document.querySelector(selector);
+
+				if (!element && hasRetries) {
+					retriesCount++;
+					return queryElement();
+				}
+
+				return resolve(element);
+			}, 250);
+		};
+
+		queryElement();
+	});
 }
