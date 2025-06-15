4bb0484c8f28b9238fbc456ebe80f129c36cd9b8
Belar <belar@belardesign.com>
2025-06-15 09:52:00 +0000

    feat: add handling of failed exports

--- src/shared/file.js
+++ src/shared/file.js
@@ -5,3 +5,33 @@ export const FILE_EVENTS = Object.freeze({
 	PREPARE_PATH: "file:prepare-path",
 	EXPORT: "file:export",
 });
+
+/**
+ * @typedef {Object} File
+ * @property {string} name - The name of the file.
+ * @property {string} projectName - The name of the project.
+ * @property {ArrayBuffer =} data - The file data.
+ */
+
+/**
+ * @param {Object} obj
+ *
+ * @returns {obj is File}
+ */
+export function isFile(obj) {
+	return (
+		!!obj &&
+		typeof obj === "object" &&
+		Object.hasOwn(obj, "name") &&
+		Object.hasOwn(obj, "projectName")
+	);
+}
+
+/**
+ * @param {Array<Object>} arr
+ *
+ * @returns {arr is Array<File>}
+ */
+export function isArrayOfFiles(arr) {
+	return Array.isArray(arr) && arr.every(isFile);
+}
